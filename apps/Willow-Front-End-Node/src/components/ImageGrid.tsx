e08b163459d633a2adea7d525c7bcb2a1c7d1df6
Φ Bui <bilphui@gmail.com>
2023-10-23 14:43:18 -0700

    restructured node modules

---
+++ apps/Willow-Front-End-Node/src/components/ImageGrid.tsx
@@ -0,0 +1,55 @@
+import { Button, Modal, Box } from "@mui/material";
+import { useState } from "react";
+
+export default function ImageGrid({ images }: { images: string[] | null }) {
+  const [open, setOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  return (
+    <Box>
+      <Box columns={3} spacing={1}>
+        {images &&
+          images.map((image, index) => (
+            <Box key={index}>
+              <img
+                src={image}
+                alt={`Image ${index}`}
+                style={{ width: "100%" }}
+              />
+            </Box>
+          ))}
+      </Box>
+      <Button onClick={() => setOpen(true)}>See All Images</Button>
+
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+          setSelectedImage(null);
+        }}
+      >
+        {selectedImage ? (
+          <img
+            src={selectedImage}
+            alt="Selected"
+            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+            onClick={() => setSelectedImage(null)}
+          />
+        ) : (
+          <Box>
+            {images &&
+              images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Image ${index}`}
+                  style={{ width: "33%", cursor: "pointer" }}
+                  onClick={() => setSelectedImage(image)}
+                />
+              ))}
+          </Box>
+        )}
+      </Modal>
+    </Box>
+  );
+}
